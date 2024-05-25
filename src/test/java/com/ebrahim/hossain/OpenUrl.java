2f263b787fda857b34bfdaf50e1792e27794abc2
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-05-25 12:07:02 -0400

    1st class

---
+++ src/test/java/com/ebrahim/hossain/OpenUrl.java
@@ -0,0 +1,12 @@
+package com.ebrahim.hossain;
+
+import org.testng.annotations.Test;
+
+public class OpenUrl extends BaseDriver{
+	
+	@Test
+	public void open() throws InterruptedException {
+		driver.get("https://www.selenium.dev/downloads/");	
+		Thread.sleep(5000);
+	}
+}
